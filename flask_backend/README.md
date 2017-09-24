990d79bccdee3c5eb62abae50f6879eedb8fb67f
David Chen <davidzchen@utexas.edu>
2017-09-24 08:42:06 -0500

    finsihed flask backend

---
+++ flask_backend/README.md
@@ -0,0 +1,15 @@
+# This is our folder for our predictive snack food consumption trend
+After we get a list of food categories the user will like, through various
+data sources, we just make a prediction based on length of road trip
+and other factors to estimate how many snacks people eat on an average
+road trip
+
+### Sheet Organization
+Sheet 1: General Numbers
+Sheet 2: Overview of sums
+Sheet 3: Salty Snacks
+Sheet 4: Sweet snacks
+Sheet 5: Meaty snacks
+Sheet 6: Granola Bars/energy bars
+
+http://www.cspdailynews.com/category-data/cmh/snacks/snacks-salty-snacks-2016
---
+++ flask_backend/itemarrays.py
@@ -0,0 +1,30 @@
+import xlrd
+book = xlrd.open_workbook("snack_data.xlsx")
+
+def getSalty():
+	sh = book.sheet_by_index(2)
+	array = [];
+	for rx in range(1, sh.nrows):
+	    array.append(sh.cell(rx,2).value)
+	return array
+
+def getSweet():
+	sh = book.sheet_by_index(3)
+	array = [];
+	for rx in range(1, sh.nrows):
+	    array.append(sh.cell(rx,2).value)
+	return array
+
+def getMeat():
+	sh = book.sheet_by_index(4)
+	array = [];
+	for rx in range(1, sh.nrows):
+	    array.append(sh.cell(rx,2).value)
+	return array
+
+def getHealthy():
+	sh = book.sheet_by_index(5)
+	array = [];
+	for rx in range(1, sh.nrows):
+	    array.append(sh.cell(rx,2).value)
+	return array
